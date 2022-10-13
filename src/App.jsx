4d030afa9e7f228082d313c0578c80b10842558b
Usman Khan <uxman302@gmail.com>
2022-10-13 16:08:29 +0500

    Added all code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Cards from "./components/Cards";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Cards />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-[#20354b] text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
+      <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+        <div class="container-fluid">
+          <a class="text-xl text-white font-semibold" href="#">
+            Bytewise Memebers
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
