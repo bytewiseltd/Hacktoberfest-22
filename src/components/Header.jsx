19cd8c01d3404c2ce187cefb4df1cfc922698699
Samar-Ali666 <sammar.ali2439@gmail.com>
2022-10-17 17:29:46 +0500

    title changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <nav class="relative w-full flex flex-wrap items-center justify-between py-3 bg-[#20354b] text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg">
       <div class="container-fluid w-full flex flex-wrap items-center justify-between px-6">
         <div class="container-fluid">
-          <a class="text-xl text-white font-semibold" href="#">
+          <a class="text-xl text-white font-semibold text-center" href="#">
             Hall Of Open Source
           </a>
         </div>
