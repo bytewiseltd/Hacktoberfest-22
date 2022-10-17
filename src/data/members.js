57fafab754d170d270eba694a65aac7ea76107a2
Usman Khan <uxman302@gmail.com>
2022-10-17 16:31:08 +0500

    Removed Extra members and Modified Readme

--- src/data/members.js
+++ src/data/members.js
@@ -1,89 +1,17 @@
 const imgPath = "/assets/members";
 const members = [
   {
-    img: `${imgPath}/team-1.jpg`,
-    firstname: "John",
-    lastname: "Doe",
-    team: "Cyber Security",
+    img: `${imgPath}/usman.jpeg`,
+    firstname: "Usman",
+    lastname: "Khan",
+    team: "Founder",
     city: "Islamabad",
     socialUrls: {
-      facebook: "https://www.facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
+      facebook: "https://www.facebook.com/usmanniazi99",
+      instagram: "https://intagram.com/_usmanniazi",
+      linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  },
-  {
-    img: `${imgPath}/team-2.jpg`,
-    firstname: "Rico",
-    lastname: "Suave",
-    team: "Frontend",
-    city: "Lahore",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
-  {
-    img: `${imgPath}/team-3.jpg`,
-    firstname: "Ben",
-    lastname: "Williams",
-    team: "Data Science",
-    city: "Karachi",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
-  {
-    img: `${imgPath}/team-4.jpg`,
-    firstname: "Kane",
-    lastname: "Williamson",
-    team: "Frontend",
-    city: "UK",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
-  {
-    img: `${imgPath}/team-5.jpg`,
-    firstname: "Jenny",
-    lastname: "Wane",
-    team: "Frontend",
-    city: "DC",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
-  {
-    img: `${imgPath}/team-6.jpg`,
-    firstname: "Denny",
-    lastname: "Root",
-    team: "Fullstack",
-    city: "USA",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
-  {
-    img: `${imgPath}/team-7.jpg`,
-    firstname: "Jimmy",
-    lastname: "Nisham",
-    team: "Data Engineer",
-    city: "UK",
-    socialUrls: {
-      facebook: "https://facebook.com",
-      instagram: "https://intagram.com",
-      linkedIn: "https://linkedin.com",
-    },
-  },
+  }
 ];
 
 export default members;
