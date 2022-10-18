ae93d7fc76dbec7fb4c8826cc4aa1ef4de8d5cde
ismail70941 <muhammadismail70941@gmail.com>
2022-10-18 13:30:00 +0500

    Picture Added - Shadow Monarch

--- src/data/members.js
+++ src/data/members.js
@@ -11,6 +11,18 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
+    {
+      img: `${imgPath}/todoroki.png`,
+      firstname: "Shadow",
+      lastname: "Monarch",
+      team: "Shadow World",
+      city: "Islamabad",
+      socialUrls: {
+        facebook: "https://www.facebook.com",
+        instagram: "https://intagram.com",
+        linkedIn: "https://www.linkedin.com/in/muhammad-i-b89a47136/",
+      },
+    },
   }
 ];
 
