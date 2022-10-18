446d7141812e99df22c6492a7d0c9215209b0ec6
Daoud-Hussain <daoudhussain302@gmail.com>
2022-10-18 13:34:46 +0500

    Useful modifications

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,19 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },
+  {
+    img: `${imgPath}/daoud.jpg`,
+    firstname: "Daoud",
+    lastname: "Hussain",
+    team: "FrontEnd Developer",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com/nadan.daoud",
+      instagram: "https://www.instagram.com/daoud_huxxain/",
+      linkedIn: "https://www.linkedin.com/in/daoud-hussain/",
+    },
+  },
 ];
 
 export default members;
