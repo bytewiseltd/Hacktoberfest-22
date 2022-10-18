5f3ee307153774203ebac424e40afe45b8444775
NoorFatima01 <nfatima101204@gmail.com>
2022-10-18 13:32:16 +0500

    Added Data - graph

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,21 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },
+  {
+    img: `${imgPath}/graph.png`,
+    firstname: "Noor",
+    lastname: "Fatima",
+    team: "Cyber Security",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://intagram.com",
+      linkedIn: "https://linkedin.com",
+    },
+  },
+  
 ];
 
 export default members;
+
