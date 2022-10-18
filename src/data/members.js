23b2a2a9dc49e7ab90006cbb600e045132d5786c
Usman Waris <usmanwaris0987@gmail.com>
2022-10-18 13:36:56 +0500

    Added data - UsmanWaris

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,18 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },{
+    img: `${imgPath}/UsmanWaris.jpeg`,
+    firstname: "Usman",
+    lastname: "Waris",
+    team: "Computer Science",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com/usman.waris.127",
+      instagram: "https://intagram.com/u.s.m.a.n__waris",
+      linkedIn: "https://www.linkedin.com/in/usman-waris-46a77b1b5/",
+    },
+  },
 ];
 
 export default members;
