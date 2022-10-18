febe45d94c8eb0434ab96e44538621c3b8f17738
Usman Khan <uxman302@gmail.com>
2022-10-18 13:29:34 +0500

    Added Data - Hamza

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,18 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },{
+    img: `${imgPath}/hamza.jpg`,
+    firstname: "Hamza",
+    lastname: "Ali",
+    team: "Cyber Security",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://intagram.com",
+      linkedIn: "https://linkedin.com",
+    },
+  },
 ];
 
 export default members;
