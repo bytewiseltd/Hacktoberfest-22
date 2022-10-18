2b3f18a92a00d5f14f94cf9117c99862ecbef64b
Muzan787 <muazahmad787@gmail.com>
2022-10-18 13:51:59 +0500

    Added Data - Muaz

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,18 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },{
+    img: `${imgPath}/muaz.jpg`,
+    firstname: "Muaz",
+    lastname: "Ahmad",
+    team: "Cyber Security",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com/MUAZ787",
+      instagram: "https://intagram.com/muaz_787",
+      linkedIn: "https://linkedin.com/in/muaz-ahmad-917184239/",
+    },
+  },
 ];
 
 export default members;
