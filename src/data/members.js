56632fa80c08568d713d9e01dc69f73a8327ced6
Mk-Muzzammil <muzzammil759@gmail.com>
2022-10-18 01:30:40 -0700

    Adding my picture

--- src/data/members.js
+++ src/data/members.js
@@ -11,7 +11,18 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
-  }
+  },{
+    img: `${imgPath}/Muzzammil.jpg`,
+    firstname: "Muhammad",
+    lastname: "Muzzammil",
+    team: "Computer Science",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://intagram.com",
+      linkedIn: "https://linkedin.com",
+    },
+  },
 ];
 
 export default members;
