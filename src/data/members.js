d12834736e3b3fd1344e3cf7466b6f5f5bbba51e
Suga7772 <numl-s22-68862@numls.edu.pk>
2022-10-18 13:29:21 +0500

    add mahrukh

--- src/data/members.js
+++ src/data/members.js
@@ -11,6 +11,19 @@ const members = [
       instagram: "https://intagram.com/_usmanniazi",
       linkedIn: "https://linkedin.com/in/usmanniazi99",
     },
+  },
+  {
+    //GDSC NUML
+    img: `${imgPath}/mahrukh.jpeg`,
+    firstname: "Mahrukh",
+    lastname: "Khan",
+    team: "Machine Lerning|Game development|Ducks",
+    city: "Islamabad",
+    socialUrls: {
+      facebook: "https://www.facebook.com/lynxmaho",
+      instagram: "https://www.instagram.com/taciturn.thing/?next=%2F",
+      linkedIn: "https://www.linkedin.com/in/mahrukh-khan-85a2711b3",
+    },
   }
 ];
 
